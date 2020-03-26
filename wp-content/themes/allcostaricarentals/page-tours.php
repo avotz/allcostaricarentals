91c9e87ddbfc2cede32236cc212521931c54bacc
Alonso Chavarria <alonso@avotz.com>
2020-03-26 10:29:11 -0600

    fix pdi

--- wp-content/themes/allcostaricarentals/page-tours.php
+++ wp-content/themes/allcostaricarentals/page-tours.php
@@ -181,7 +181,7 @@ get_header(); ?>
 												</h4>
 											</div>
 											<div class="rating">
-												<?php if (function_exists("kk_star_ratings")) : echo kk_star_ratings($pid);
+												<?php if (function_exists("kk_star_ratings")) : echo kk_star_ratings($post->ID);
 												endif; ?>
 											</div>
 											<div class="price">
